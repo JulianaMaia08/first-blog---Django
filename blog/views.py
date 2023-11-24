2d1b75237dacd36437172c89d627e04989815136
Juliana Maia <juuh.maia080196@uni9.edu.br>
2023-11-24 11:31:12 -0300

    Adicionando moelos no template

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
-
+from django.views.generic.list import ListView
+from django.utils import timezone
+from .models import Post
 # Create your views here.
+
+class PostList(ListView):
+    template_name = "post_list.html"
+    model = Post
+
