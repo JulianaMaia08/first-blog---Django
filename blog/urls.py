8c2759283676647b117d008de6950f1a692c4852
Juliana Maia <juuh.maia080196@uni9.edu.br>
2023-12-01 10:41:45 -0300

    Add view to create post

--- blog/urls.py
+++ blog/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
-from .views import PostList, PostDetail
+from .views import PostList, PostDetail, NewPost, ThanksPostView
 
 app_name = "blog"
 
 urlpatterns = [
     path('', PostList.as_view(), name='post_list'),
     path('post/<int:pk>/', PostDetail.as_view(), name='post_detail'),
+    path('post/new', NewPost.as_view(), name='post_new'),
+    path('post/thanks_post/', ThanksPostView.as_view(), name='thanks_post'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,9 @@
-from django.shortcuts import render
-from django.views.generic import ListView, DetailView
+from django.shortcuts import render, redirect
+from django.urls import reverse_lazy
+from django.views.generic import ListView, DetailView, FormView, TemplateView
 from django.utils import timezone
 from .models import Post
+from .forms import PostForm
 # Create your views here.
 
 class PostList(ListView):
@@ -11,3 +13,21 @@ class PostList(ListView):
 class PostDetail(DetailView):
     template_name = "post_detail.html"
     model = Post
+
+class ThanksPostView(TemplateView):
+    template_name = 'thanks_post.html'
+
+class NewPost(FormView):
+    form_class = PostForm
+    template_name = "post_edit.html"
+
+
+    def form_valid(self, form):
+        # O formulário é válido, faça algo com os dados
+        post = form.save(commit=False)
+        post.author = self.request.user
+        post.save()
+        return redirect('blog:post_detail', pk=post.pk)
+
+        return super().form_valid(form)
+
